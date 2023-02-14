10a0ea2e46b2a2aea0a5a2c7b08c60f7f1687fde
John Atkinson <johnatkinson2020@gmail.com>
2023-02-14 13:50:52 -0500

    Added comments

--- ABCDEFGH/ABCDMain.java
+++ ABCDEFGH/ABCDMain.java
@@ -1,13 +1,15 @@
-//Client program to use the A, B, C, and D classes
+//Demonstrates how to use inheritances with Client program to use the A, B, C, and D classes
 
 public class ABCDMain {
     public static void main(String[] args) {
         A[] elements = {new A(), new B(), new C(), new D()};
 
+        //will print through each class, follow along to understand inheritance
         for (int i = 0; i < elements.length; i++) {
             System.out.println(elements[i]);
             elements[i].method1();
             elements[i].method2();
             System.out.println();
+        }
     }
 }
--- ABCDEFGH/EFGHMain.java
+++ ABCDEFGH/EFGHMain.java
@@ -1,5 +1,15 @@
+//Demonstrates how to use inheritances with Client program to use the E, F, G, and H classes
+
 public class EFGHMain {
     public static void main(String[] args) {
-        //TODO
+        G[] elements = {new E(), new F(), new G(), new H()};
+
+        //will print through each class, follow along to understand inheritance
+        for (int i = 0; i < elements.length; i++) {
+            System.out.println(elements[i]);
+            elements[i].method1();
+            elements[i].method2();
+            System.out.println();
+        }
     }
 }
