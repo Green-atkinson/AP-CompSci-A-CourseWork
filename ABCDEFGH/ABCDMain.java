2bb66caed1ca1b0a9b07543c8f2a7f8ed4ac14a7
John Atkinson <johnatkinson2020@gmail.com>
2023-02-14 13:29:43 -0500

    Update ABCDMain.java

--- ABCDEFGH/ABCDMain.java
+++ ABCDEFGH/ABCDMain.java
@@ -9,17 +9,5 @@ public class ABCDMain {
             elements[i].method1();
             elements[i].method2();
             System.out.println();
-        }
-        
-        String inputString = "acbca";
-        
-        String opposite = "";
-        
-        //Add last char index to new string 'opposite'
-        for(int i = inputString.length() - 1; i >= 0; i--) {
-            opposite += inputString.charAt(i);
-        }
-        System.out.println(inputString);
-        System.out.println(opposite);
     }
 }
